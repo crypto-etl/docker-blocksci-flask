963489b701521bbf9a0a8c9ab4253114825a6329
Nirmal AK <aknirmal90@gmail.com>
2018-11-11 10:09:08 +0800

    adding transaction serializer

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from flask import Flask, jsonify, request
 
 import blocksci
-from serializer import BlockSerializer
+from serializer import BlockSerializer, TransactionSerializer
 
 
 blockchain = blocksci.Blockchain('/blocksci/blocksci-parser/')
@@ -12,11 +12,22 @@ app = Flask(__name__)
 
 
 def get_blockrange(start, end):
+    """
+    returns a blocksci.BlockIterator with blocks within the
+    indicated (start, end) block heights or block times
+
+    param: start (int, str)
+        block height or block times
+    param: end (int, str)
+        block height or block times
+    """
     if start.isdigit() and end.isdigit():
         start, end = int(start), int(end)
-        blocks = blockchain[start:end]
+        blocks = blockchain[start:end]  #  Blocksci does not raise an IndexError in case slice indexes are out of range
     elif isinstance(start, str) and isinstance(end, str):
         blocks = blockchain.range(start=start, end=end)
+        # Blocksci raises an IndexError in case start end_date is before date of genesis block
+        raise IndexError('End Date for range is prior to Genesis block')
     else:
         raise ValueError('Allowed Values for `start` and `end` are `integers` and `date strings`')
     return blocks
@@ -24,18 +35,40 @@ def get_blockrange(start, end):
 
 @app.route('/block/<height>', methods=['GET'])
 def serve_block(height):
+    """
+    returns a json serialized blocksci.Block object
+
+    ------------------------
+    Parameters passed in URL
+    ------------------------
+
+    param: height (int)
+        block height
+    """
     if not height.isdigit():
         return jsonify(data='Invalid argument: Block Height must be an integer')
 
     if int(height) > len(blockchain) - 1:
-        return jsonify(data='Invalid argument: Block at given Height not found')        
+        return jsonify(data='Invalid argument: Block at given Height not found')
 
-    block = blockchain[int(height)]        
+    block = blockchain[int(height)]
     return jsonify(data=BlockSerializer.serialize(block))
 
 
 @app.route('/block/list', methods=['GET'])
 def serve_block_list():
+    """
+    returns a list of json serialized blocksci.Block objects
+
+    ---------------------------------
+    Parameters passed in Request Body
+    ---------------------------------
+
+    param: start (int, str)
+        block height or a string which can be parsed to a datetime.datetime object
+    param: end (int, str)
+        block height or a string which can be parsed to a datetime.datetime object
+    """
     start, end = request.values.get('start'), request.values.get('end')
 
     if start is None or end is None:
@@ -43,11 +76,28 @@ def serve_block_list():
 
     try:
         blocks = get_blockrange(start=start, end=end)
-    except ValueError as e:
-        return jsonify(data=e.message)
-    return jsonify(data=[BlockSerializer.serialize(_block) for _block in blocks])
+        return jsonify(data=[BlockSerializer.serialize(_block) for _block in blocks])
+    except (IndexError, ValueError) as e:
+        return jsonify(data=str(e))
 
 
-@app.route('/transactions', methods=['GET'])
-def serve_transaction():
-    pass
+@app.route('/transactions/<_hash>', methods=['GET'])
+def serve_transaction(_hash):
+    """
+    returns a json serialized blocksci.Tx object
+
+    ------------------------
+    Parameters passed in URL
+    ------------------------
+
+    param: _hash (str)
+        Transaction Hash
+    """
+    if len(_hash) != 64:
+        return jsonify(data='Invalid argument: TxHash must be 64 chars')
+
+    try:
+        tx = blockchain.tx_with_hash(_hash)
+        return jsonify(data=TransactionSerializer.serialize(tx))
+    except RuntimeError:
+        return jsonify(data=str(e))
